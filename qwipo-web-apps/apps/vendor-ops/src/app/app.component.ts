a8025c1c3b3b57d41aa1ae34e4cdec73b5d13246
kranthikiran <46471488+kranthi-kiki@users.noreply.github.com>
2021-05-19 23:11:48 +0530

    added home routing module

--- qwipo-web-apps/apps/vendor-ops/src/app/app.component.ts
+++ qwipo-web-apps/apps/vendor-ops/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'qwipo-web-apps-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'vendor-ops';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    console.log('this is app');
+    // this.router.navigate(['/home']);
+    // this.router.navigate(['/authentication/login']);
+  }
 }

aa48451d6596f9b273d67daa46b83039827ea881
kranthikiran <46471488+kranthi-kiki@users.noreply.github.com>
2021-06-12 16:37:59 +0530

    Update home.component.ts

--- qwipo-web-apps/apps/vendor-ops/src/app/pages/home/home.component.ts
+++ qwipo-web-apps/apps/vendor-ops/src/app/pages/home/home.component.ts
@@ -7,29 +7,10 @@ import { AngularFireDatabase } from '@angular/fire/database';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  database: any;
   constructor(private db: AngularFireDatabase) {}
 
   async ngOnInit() {
     console.log('this is home');
-    await this.getOrders();
   }
 
-  private async getOrders() {
-    const mainOrders = [];
-    // eslint-disable-next-line no-var
-    var path = `Orders`;
-    return this.db.database
-      .ref(path)
-      .once('value')
-      .then((snapshot) => {
-        const snapCustomer = snapshot.val();
-        for (const key in snapCustomer) {
-          const order = snapCustomer[key];
-          mainOrders.push(order);
-        }
-        console.log('orders:::', mainOrders[0]);
-        return mainOrders;
-      });
-  }
 }

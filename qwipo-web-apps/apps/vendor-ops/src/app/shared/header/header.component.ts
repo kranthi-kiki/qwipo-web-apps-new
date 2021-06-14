e630968a1cd7a52c3b49f7b27c96ef38cb6ad3c2
kranthikiran <46471488+kranthi-kiki@users.noreply.github.com>
2021-06-14 21:47:42 +0530

    added header component

---
+++ qwipo-web-apps/apps/vendor-ops/src/app/shared/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qwipo-web-apps-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- qwipo-web-apps/apps/vendor-ops/src/app/shared/shared.module.ts
+++ qwipo-web-apps/apps/vendor-ops/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
     SidebarComponent,
-    LayoutComponent
+    LayoutComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule

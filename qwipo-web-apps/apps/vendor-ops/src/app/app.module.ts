ddb943a6bdb656560e93fa73faf4d46dcf05e1e2
kranthikiran <46471488+kranthi-kiki@users.noreply.github.com>
2021-06-14 21:57:04 +0530

    Update app.module.ts

--- qwipo-web-apps/apps/vendor-ops/src/app/app.module.ts
+++ qwipo-web-apps/apps/vendor-ops/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
+    AuthModule,
     BrowserAnimationsModule,
     StoreModule.forRoot({}, {}),
     AngularFireModule.initializeApp(environment.firebaseConfig),

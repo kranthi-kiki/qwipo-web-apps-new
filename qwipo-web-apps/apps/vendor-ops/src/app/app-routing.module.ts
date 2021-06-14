1fecfbb65ae6f52cd27f58fc2ef6d1d35193fdcb
kranthikiran <46471488+kranthi-kiki@users.noreply.github.com>
2021-06-14 08:21:38 +0530

    changes

--- qwipo-web-apps/apps/vendor-ops/src/app/app-routing.module.ts
+++ qwipo-web-apps/apps/vendor-ops/src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-
+  { path: '', redirectTo: 'pages/home', pathMatch: 'full' },
   {
     path: 'pages',
     loadChildren: () =>
@@ -12,7 +11,9 @@ const routes: Routes = [
   {
     path: 'auth', loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule)
   },
-  { path: '**', redirectTo: 'pages/home' },
+  // { path: '**', redirectTo: 'pages/home' }
+
+
 ];
 const config: ExtraOptions = {
   useHash: true,
--- qwipo-web-apps/apps/vendor-ops/src/app/pages/pages-routing.module.ts
+++ qwipo-web-apps/apps/vendor-ops/src/app/pages/pages-routing.module.ts
@@ -1,12 +1,20 @@
+import { PagesComponent } from './pages.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { APP_ROUTES } from '../constants/app-routes';
 
 const routes: Routes = [
   {
-    path: APP_ROUTES.HOME, loadChildren: () =>
+    path: '', component: PagesComponent,
+    children: [
+      {
+        path: APP_ROUTES.HOME, loadChildren: () =>
       import('./home/home.module').then((m) => m.HomeModule)
-  }
+      }
+    ]
+  },
+
+
 ];
 
 @NgModule({

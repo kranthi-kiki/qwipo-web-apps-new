76ef29370e365788ac79716413eb42471a543ce3
kranthikiran <46471488+kranthi-kiki@users.noreply.github.com>
2021-06-14 21:45:22 +0530

    added app layout

--- qwipo-web-apps/apps/vendor-ops/src/app/shared/shared.module.ts
+++ qwipo-web-apps/apps/vendor-ops/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    LayoutComponent
   ],
   imports: [
     CommonModule
